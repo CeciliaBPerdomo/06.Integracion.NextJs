755d5f9b4894d8ae1d1d86856e4d7b8150396f26
Cecilia Perdomo <cecilia.perdomo@gmail.com>
2024-02-14 15:27:53 -0300

    Modulo 06

--- src/app/page.js
+++ src/app/page.js
@@ -3,19 +3,20 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="container m-auto">
-      <h1 className="text-4xl">Módulo 5: Data Fetching II</h1>
+      <h1 className="text-4xl">Módulo 6: Integración</h1>
       <hr />
       <br />
       <ol className="text-2xl ml-8">
-        <li> - 5.1: Loading UI</li>
-        <li> - 5.2: Streaming y Suspense</li>
-        <li> - 5.3: Integrando Firebase</li>
-        <li> - 5.4: Firestore</li>
-        <li> - 5.5: Consumiendo Firestore</li>
-        <li> - 5.6: generateStaticParams</li>
-        <li> - 5.7: Context y Providers</li>
-        <li> - 5.8: Recomendaciones para el Proyecto Final V</li>
-        <li> - 5.9: Optimización de Next.js con Firebase</li>
+        <li> - 6.1: Integraciones</li>
+        <li> - 6.2: Firebase / Storage</li>
+        <li> - 6.3: Creación: Storage + Firebase</li>
+        <li> - 6.4: AuthContext</li>
+        <li> - 6.5: Parallel Routes</li>
+        <li> - 6.6: Firebase Auth</li>
+        <li> - 6.7: Registro y Login</li>
+        <li> - 6.8: Observador de estado</li>
+        <li> - 6.9: Google Auth Provider</li>
+        <li> - 6.10: Recomendaciones para el proyecto final VI</li>
       </ol>
       <br />
       <hr />
